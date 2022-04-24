3c60376b9f4104981e7f5293ecabfb80e714118d
Steven <stevenjohnlarsen@gmail.com>
2022-04-24 12:37:27 -0500

    pushing up before working separetly

--- helpers/helpers.R
+++ helpers/helpers.R
@@ -8,3 +8,87 @@ train_test_split <- function(data, train_percent, test_percent){
   test <- data[split == 1, ]   
   list(train, test) 
 }
+
+get_subset_1 <- function(data) {
+  data$risk_cases_numbers <- sapply(data$risk_cases, unclass)
+  data$risk_cases_numbers <- as.factor(data$risk_cases_numbers)
+  
+  select(data, 
+         total_pop,
+         median_age,
+         white_pop,
+         black_pop,
+         asian_pop,
+         hispanic_pop,
+         amerindian_pop,
+         other_race_pop,
+         two_or_more_races_pop,
+         risk_cases_numbers, 
+         median_income,
+         median_rent,
+         percent_income_spent_on_rent,
+         high_school_diploma,
+         less_one_year_college,
+         bachelors_degree,
+         masters_degree,
+         risk_cases_numbers)
+}
+
+
+get_subset_2 <- function(data) {
+  data$risk_cases_numbers <- sapply(data$risk_cases, unclass)
+  data$risk_cases_numbers <- as.factor(data$risk_cases_numbers)
+  
+  select(data, 
+         median_income,
+         median_rent,
+         percent_income_spent_on_rent,
+         high_school_diploma,
+         less_one_year_college,
+         bachelors_degree,
+         masters_degree,
+         risk_cases_numbers)
+}
+
+
+get_subset_3 <- function(data) {
+  data$risk_cases_numbers <- sapply(data$risk_cases, unclass)
+  data$risk_cases_numbers <- as.factor(data$risk_cases_numbers)
+  
+  select(data, 
+         commute_less_than_30,
+         commute_30_60,
+         commute_more_than_60,
+         commuters_by_public_transportation,
+         total_pop,
+         median_age,
+         risk_cases_numbers)
+}
+
+get_subset_all <- function(data) {
+  data$risk_cases_numbers <- sapply(data$risk_cases, unclass)
+  data$risk_cases_numbers <- as.factor(data$risk_cases_numbers)
+  
+  select(data, 
+         total_pop,
+         median_age,
+         white_pop,
+         black_pop,
+         asian_pop,
+         hispanic_pop,
+         amerindian_pop,
+         other_race_pop,
+         two_or_more_races_pop,
+         commuters_by_public_transportation,
+         median_income,
+         median_rent,
+         percent_income_spent_on_rent,
+         high_school_diploma,
+         less_one_year_college,
+         bachelors_degree,
+         masters_degree,
+         commute_less_than_30,
+         commute_30_60,
+         commute_more_than_60,
+         risk_cases_numbers)
+}
